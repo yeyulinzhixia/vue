e3b6636f5b35a883d365902cd84b960ab1cbdc9d
yeyulinzhixia <479892367@qq.com>
2020-02-16 23:37:36 +0800

    master

---
+++ vue-admin/src/views/Login/index.vue
@@ -0,0 +1,18 @@
+<template>
+<div id="login">这里是登陆页
+    <el-button>默认按钮</el-button>
+</div>
+</template>
+<script>
+export default{
+    name:'login'
+}
+</script>
+<style lang="scss" scoped>
+#login {
+    background-color:#344a5f;
+    height: 100vh; 
+}
+
+
+</style>
